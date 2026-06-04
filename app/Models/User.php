43da4062d04c1457b7569af6e7d1984703a15572
Sujal Savsani <savasanisujal1@gmail.com>
2026-06-04 15:12:34 +0530

    add subscription analytics dashboard, invoice tracking, and coupon support

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use LucasDotVin\Soulbscription\Models\Concerns\HasSubscriptions; // THIS IS REQUIRED
+use LucasDotVin\Soulbscription\Models\Concerns\HasSubscriptions;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasSubscriptions;
+
     protected $fillable = [
         'name',
         'email',
@@ -20,4 +21,15 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function checkFeature($featureName)
+    {
+       $subscription = $this->subscription;
+
+    if (!$subscription) {
+        return null;
+    }
+
+    return $subscription->plan->features()->where('name', $featureName)->first();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\Admin\AnalyticsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/subscribe/{user}', [SubscriptionController::class, 'create']);
-Route::get('/subscriptions/{user}', [SubscriptionController::class, 'list']);   
+Route::get('/subscriptions/{user}', [SubscriptionController::class, 'list']); 
 
 Route::get('/feature-check/{user}', [SubscriptionController::class, 'checkFeature']);
 
 Route::get('/cancel/{user}', [SubscriptionController::class, 'cancel']);
+
+Route::get('/admin/analytics', [AnalyticsController::class, 'index']);
